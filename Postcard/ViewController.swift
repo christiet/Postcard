65f5ca2a08a30d968e5ea1c3f6f9ab8053516836
Tony Christie <tony.christie@gmail.com>
2014-09-14 10:12:46 +0100

    Add name label

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     
     @IBOutlet weak var enterMessageTextField: UITextField!
@@ -41,7 +43,10 @@ class ViewController: UIViewController {
         messageLabel.textColor = UIColor.redColor()
         sendMailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         messageLabel.hidden = false
-        
+        nameLabel.text = enterNameTextField.text
+        enterNameTextField.text = ""
+        nameLabel.textColor = UIColor.blueColor()
+        nameLabel.hidden = false
     }
 }
 
